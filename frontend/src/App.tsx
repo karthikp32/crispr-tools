8364ba6335b8bd050aaaa4d5c763e0936b769b19
Karthik Pillalamarri <karthik.pillalamarri.kp@gmail.com>
2025-07-20 15:35:22 -0500

    Revert "feat: add vercel analytics and fix build errors"
    This reverts commit 17b2ab78c5300e91e356dac2596f0acc8ea3f5ea.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,13 +2,11 @@ import { Suspense } from "react";
 import { useRoutes, Routes, Route } from "react-router-dom";
 import Home from "./components/home";
 import routes from "tempo-routes";
-import { Analytics } from "@vercel/analytics/react";
 
 function App() {
   return (
     <Suspense fallback={<p>Loading...</p>}>
       <>
-        <Analytics />
         <Routes>
           <Route path="/" element={<Home />} />
         </Routes>
